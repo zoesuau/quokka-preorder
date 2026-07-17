d3f45f807e2a663f6733ff81ec40645f5d3029fd
zoe <zoesuau@gmail.com>
2026-07-17 16:42:02 +0800

    Enable frontend LIFF login

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.QUOKKA_CONFIG = {
   // 部署 Code.gs 為 GAS 網頁應用程式後，貼上 /exec 網址。
   apiUrl: "https://script.google.com/macros/s/AKfycbzh7CqO-u_2gRWvlvUPzOAE7bAEQOczgmO18DZq7XbmEU2BzZQJetXn6MzzmsxiZbAF/exec",
   // LINE Developers 建立 LIFF App 後貼上 LIFF ID。前台與後台可共用同一個 LIFF。
-  liffId: "",
+  liffId: "2010743514-C2ckA5v9",
   // 建議後台另建一個 Endpoint 指向 admin.html；留空時會沿用上方 LIFF ID。
   adminLiffId: "",
   brandName: "鼠購易｜韓國小物預購",
